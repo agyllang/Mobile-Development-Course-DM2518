7909559e5a619b976fe3254d8c916be7404c9bdd
agyllang <46810941+agyllang@users.noreply.github.com>
2020-04-08 15:59:00 +0200

    alot of stuff yo

---
+++ index.js
@@ -0,0 +1,32 @@
+document.addEventListener("init", function (event) {
+  var page = event.target;
+  console.log("page: ", page);
+
+  if (page.id === "home") {
+    page.querySelector("#push-button").onclick = function () {
+      document
+        .querySelector("#myNavigator")
+        .pushPage("instructions.html", { data: { title: "Instructions" } });
+    };
+
+    page.querySelector("#relax").onclick = function () {
+      document
+        .querySelector("#myNavigator")
+        .pushPage("relax.html", { data: { title: "Relax" } });
+    };
+
+    page.querySelector("#discover").onclick = function () {
+      document
+        .querySelector("#myNavigator")
+        .pushPage("discover.html", { data: { title: "Discover" } });
+    };
+
+    page.querySelector("#party").onclick = function () {
+      document
+        .querySelector("#myNavigator")
+        .pushPage("party.html", { data: { title: "Party" } });
+    };
+  } else if (page.id === "instructions") {
+    page.querySelector("ons-toolbar .center").innerHTML = page.data.title;
+  }
+});
