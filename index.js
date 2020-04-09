da64e4d97fbda72bd32467f44598bd773256cba5
agyllang <46810941+agyllang@users.noreply.github.com>
2020-04-09 09:27:09 +0200

    Exercise 3&4

--- index.js
+++ index.js
@@ -2,37 +2,37 @@ document.addEventListener("init", function (event) {
   var page = event.target;
   console.log("page: ", page);
 
-  if (page.id === "home") {
-    page.querySelector("#push-button").onclick = function () {
-      document
-        .querySelector("#myNavigator")
-        .pushPage("instructions.html", { data: { title: "Instructions" } });
-    };
+  //   if (page.id === "home") {
+  //     page.querySelector("#push-button").onclick = function () {
+  //       document
+  //         .querySelector("#myNavigator")
+  //         .pushPage("instructions.html", { data: { title: "Instructions" } });
+  //     };
 
-    page.querySelector("#relax").onclick = function () {
-      document
-        .querySelector("#myNavigator")
-        .pushPage("relax.html", { data: { title: "Relax" } });
-    };
+  //     page.querySelector("#relax").onclick = function () {
+  //       document
+  //         .querySelector("#myNavigator")
+  //         .pushPage("relax.html", { data: { title: "Relax" } });
+  //     };
 
-    page.querySelector("#discover").onclick = function () {
-      document
-        .querySelector("#myNavigator")
-        .pushPage("discover.html", { data: { title: "Discover" } });
-    };
+  //     page.querySelector("#discover").onclick = function () {
+  //       document
+  //         .querySelector("#myNavigator")
+  //         .pushPage("discover.html", { data: { title: "Discover" } });
+  //     };
 
-    page.querySelector("#party").onclick = function () {
-      document
-        .querySelector("#myNavigator")
-        .pushPage("party.html", { data: { title: "Party" } });
-    };
-  } else if (page.id === "instructions") {
-    page.querySelector("ons-toolbar .center").innerHTML = page.data.title;
-  } else if (page.id === "relax") {
-    page.querySelector("ons-toolbar .center").innerHTML = page.data.title;
-  } else if (page.id === "discover") {
-    page.querySelector("ons-toolbar .center").innerHTML = page.data.title;
-  } else if (page.id === "party") {
-    page.querySelector("ons-toolbar .center").innerHTML = page.data.title;
-  }
+  //     page.querySelector("#party").onclick = function () {
+  //       document
+  //         .querySelector("#myNavigator")
+  //         .pushPage("party.html", { data: { title: "Party" } });
+  //     };
+  //   } else if (page.id === "instructions") {
+  //     page.querySelector("ons-toolbar .center").innerHTML = page.data.title;
+  //   } else if (page.id === "relax") {
+  //     page.querySelector("ons-toolbar .center").innerHTML = page.data.title;
+  //   } else if (page.id === "discover") {
+  //     page.querySelector("ons-toolbar .center").innerHTML = page.data.title;
+  //   } else if (page.id === "party") {
+  //     page.querySelector("ons-toolbar .center").innerHTML = page.data.title;
+  //   }
 });
